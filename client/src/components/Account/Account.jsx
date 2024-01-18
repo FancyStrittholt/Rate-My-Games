7577d34233c34937c033d4dfc053cc1c2d6bcd4e
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2024-01-17 19:26:01 -0500

    Added tailwind and headless ui and profile picture support

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -1,52 +1,151 @@
-import {useSelector} from 'react-redux';
-import {useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
-import styles from './Account.module.css';
-import GameCard from '../common/GameCard';
+import { useSelector } from "react-redux";
+import { Fragment, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Account.module.css";
+import GameCard from "../common/GameCard";
+import { Dialog, Transition, RadioGroup } from "@headlessui/react";
 
+const pics = ["lara.png", "aloy.png", 'najuma.png', 'lifeline.png', 'cirilla.png', 'commando.png', 'conner.png', 'evee.png', 'jesse.png', 'joseph.png', 'keanu.png','link.png', 'loba.png', 'sylvanas.png', 'terrorist.png', 'witcher.png'];
 
 export default function Account() {
-    const user = useSelector((it) => it.state.user);
-    const votes = useSelector((it) => it.state.votes);
-    const games = useSelector((it) => it.state.games);
+  const user = useSelector((it) => it.state.user);
+  const votes = useSelector((it) => it.state.votes);
+  const games = useSelector((it) => it.state.games);
+  let [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(pics[0]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!user?.token) {
-            navigate('/login');
-        }
-    }, []);
-
-    function createMyGames() {
-        const cards = [];
-        for (const vote of votes) {
-            const game = games.find(it => it.id === vote.gameid)
-            cards.push(
-                <GameCard key={game.id} game={game} />
-            )
-
-        }
-        return cards;
+  useEffect(() => {
+    if (!user?.token) {
+      navigate("/login");
     }
+  }, []);
 
-    return (
-        <>
-            <img
-                width={150}
-                height={150}
-                className={styles['profile-pic']}
-                src={new URL(`../../assets/images/profile/${user.pic}`, import.meta.url).href}
-            ></img>
-            <div>
-                {user.username}
-                <br />
-                {user.email}
+  function createMyGames() {
+    const cards = [];
+    for (const vote of votes) {
+      const game = games.find((it) => it.id === vote.gameid);
+      cards.push(<GameCard key={game.id} game={game} />);
+    }
+    return cards;
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <>
+      <img
+        width={150}
+        height={150}
+        className={styles["profile-pic"]}
+        src={
+          new URL(`../../assets/images/profile/${user.pic}`, import.meta.url)
+            .href
+        }
+      ></img>
+      <button onClick={() => openModal()}>Edit Profile Picture</button>
+      <div>
+        {user.username}
+        <br />
+        {user.email}
+      </div>
+      <div>
+        <h2>My fave games</h2>
+        {
+          <div className={styles["game-container"]}>
+            {votes && createMyGames()}
+          </div>
+        }
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-5xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Choose your avatar
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <RadioGroup value={selected} onChange={setSelected} className='flex flex-wrap gap-5'>
+                      {pics.map((pic) => (
+                        <RadioGroup.Option key={pic} value={pic}>
+                          {({ active, checked }) => (
+                            <>
+                              <img
+                                width={200}
+                                height={200}
+                                className={`${
+                                  active
+                                    ? "ring-2 ring-white/60 ring-offset-4 ring-offset-sky-300"
+                                    : ""
+                                }
+                                      ${
+                                        checked
+                                          ? "border-sky-900/75 text-white"
+                                          : "border-white"
+                                      }
+                                      relative flex cursor-pointer rounded-full shadow-md focus:outline-none`}
+                                src={
+                                  new URL(
+                                    `../../assets/images/profile/${pic}`,
+                                    import.meta.url
+                                  ).href
+                                }
+                              ></img>
+                            </>
+                          )}
+                        </RadioGroup.Option>
+                      ))}
+                    </RadioGroup>
+                  </div>
+
+                  <div className="mt-4 flex justify-end">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
             </div>
-            <div>
-                <h2>My fave games</h2>
-                {<div className={styles['game-container']}>{createMyGames()}</div>}
-            </div>
-        </>
-    );
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
 }
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,62 +1,61 @@
-import {useSelector} from 'react-redux';
-import {Link, useNavigate} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import styles from './Nav.module.css';
-import {updateUser} from '../../app/slice';
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { updateUser } from "../../app/slice";
 
 export default function Nav() {
-    const token = useSelector((it) => it.state.user?.token);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const token = useSelector((it) => it.state.user?.token);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    function logout() {
-        navigate('/');
-        dispatch(updateUser(''));
-    }
-    return (
-        <>
-            <div className={styles.nav}>
-                <div>
-                    <h2>Rate My Games</h2>
-                </div>
-                <div className='nav-links'>
-                    <span className='material-icons'>sports_esports</span>
-                    <Link to='/'>Games</Link>
+  function logout() {
+    navigate("/");
+    dispatch(updateUser(''));
+  }
+  return (
+    <>
+      <div className='bg-fuchsia-600 flex items-center justify-between text-[#f5f5f5] pl-[25px] pr-[25px] font-medium'>
+        <div>
+          <h2>Rate My Games</h2>
+        </div>
+        <div className="flex gap-[10px]">
+          <span className="material-icons">sports_esports</span>
+          <Link to="/">Games</Link>
 
-                    <span className='material-icons'>sports_esports</span>
-                    <Link to='/leaderboard'>Leaderboard</Link>
+          <span className="material-icons">sports_esports</span>
+          <Link to="/leaderboard">Leaderboard</Link>
 
-                    {!token && (
-                        <>
-                            <span className='material-icons'>app_registration</span>{' '}
-                            <Link to='/register'>Register</Link>
-                        </>
-                    )}
+          {!token && (
+            <>
+              <span className="material-icons">app_registration</span>{" "}
+              <Link to="/register">Register</Link>
+            </>
+          )}
 
-                    {token && (
-                        <>
-                            <span className='material-icons'>account_circle</span>
-                            <Link to='/account'>Account</Link>
-                        </>
-                    )}
+          {token && (
+            <>
+              <span className="material-icons">account_circle</span>
+              <Link to="/account">Account</Link>
+            </>
+          )}
 
-                    {!token && (
-                        <>
-                            <span className='material-icons'>login</span>
-                            <Link to='/login'>Login</Link>
-                        </>
-                    )}
+          {!token && (
+            <>
+              <span className="material-icons">login</span>
+              <Link to="/login">Login</Link>
+            </>
+          )}
 
-                    {token && (
-                        <>
-                            <span className='material-icons'>logout</span>
-                            <a onClick={() => logout()} to='/'>
-                                Logout
-                            </a>
-                        </>
-                    )}
-                </div>
-            </div>
-        </>
-    );
+          {token && (
+            <>
+              <span className="material-icons">logout</span>
+              <a onClick={() => logout()} to="/">
+                Logout
+              </a>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
