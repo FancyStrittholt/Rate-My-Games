4b5e74bcb08b41345da4c8eed93bef29b891bbc3
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2024-01-21 11:09:26 -0500

    landing page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,28 +1,6 @@
-import { Alert } from '@coreui/react';
-import '@coreui/coreui/dist/css/coreui.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "@coreui/coreui/dist/css/coreui.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-<CCarousel controls indicators dark>
-  <CCarouselItem>
-    <CImage className="d-block w-100" src={ReactImg} alt="slide 1" />
-    <CCarouselCaption className="d-none d-md-block">
-      <h5>First slide label</h5>
-      <p>Some representative placeholder content for the first slide.</p>
-    </CCarouselCaption>
-  </CCarouselItem>
-  <CCarouselItem>
-    <CImage className="d-block w-100" src={VueImg} alt="slide 2" />
-    <CCarouselCaption className="d-none d-md-block">
-      <h5>Second slide label</h5>
-      <p>Some representative placeholder content for the first slide.</p>
-    </CCarouselCaption>
-  </CCarouselItem>
-  <CCarouselItem>
-    <CImage className="d-block w-100" src={AngularImg} alt="slide 3" />
-    <CCarouselCaption className="d-none d-md-block">
-      <h5>Third slide label</h5>
-      <p>Some representative placeholder content for the first slide.</p>
-    </CCarouselCaption>
-  </CCarouselItem>
-</CCarousel>
+export default function Home() {
+  return <></>;
+}
